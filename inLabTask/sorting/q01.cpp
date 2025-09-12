276f79abd857ba0b487bf946631e38444a2de079
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2025-09-12 13:21:10 +0500

    Update q01.cpp

--- inLabTask/sorting/q01.cpp
+++ inLabTask/sorting/q01.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int *arr, int n) {
+    bool swapped = false;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                swapped = true;
+            }
+        }
+        if (swapped == false) {
+            return;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5,1,3,6,2,9,7,4,8,10};
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl << endl;
+    bubbleSort(arr, 10);
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+}
