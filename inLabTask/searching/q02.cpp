62009957d36f5a72bec3be2ed3cfdca2899cf8e0
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2025-09-12 13:18:14 +0500

    Update q02.cpp

--- inLabTask/searching/q02.cpp
+++ inLabTask/searching/q02.cpp
@@ -21,10 +21,10 @@ int binarySearch(int *arr, int target, int n) {
     return -1; 
 }
 
-void bubbleSort(int arr[], int size) {
+void bubbleSort(int arr[], int n) {
     bool swapped = false;
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
