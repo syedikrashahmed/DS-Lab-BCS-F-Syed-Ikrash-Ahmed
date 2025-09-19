386edad31e7b4aa611c098ec8263ae5cc86beca1
Syed Ikrash Ahmed <132776071+syedikrashahmed@users.noreply.github.com>
2025-09-19 11:23:10 +0500

    Update q01.cpp

--- Lab05/InLab/q01.cpp
+++ Lab05/InLab/q01.cpp
@@ -1,1 +1,22 @@
+/*Task 1:
+Write a recursive C++ function calculateFactorial(int n) that computes the factorial
+of a given integer n . The function should have a base condition to stop the recursion when n is
+0 or 1, and should call itself with a smaller value of n (e.g., n-1) to solve the problem.
+*/
+#include <iostream>
+using namespace std; 
+int calculateFactorial(int n) {
+    if (n == 0|| n==1){
+        return 1;
+    }
+    return n *calculateFactorial(n-1);
+}
 
+int main() {
+    int num;
+    cout << "Enter num to factorial:";
+    cin>> num;
+    int result = calculateFactorial(num);
+    cout<< result;
+
+}
